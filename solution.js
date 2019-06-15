82a28f436c4ce0eca40a2d975f072fa4b60bf012
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-15 16:25:39 +0200

    modify recipe code

--- solution.js
+++ solution.js
@@ -1,17 +1,25 @@
-//recipe object 
-function Recipe(title, servings, ingredients) {
-    this.title = title;
-    this.servings = servings;
-    this.ingredients = ingredients;
-    this.printRecipe = function () {
-        console.log(this.title);
-        console.log('Serves: ' + this.servings);
-        console.log('Ingredients:');
-        for(var i = 0; i < ingredients.length; i++) {
-            console.log(i + 1 + '. ' + ingredients[i]);
+//1. recipe object 
+class Recipe {
+    constructor(title, servings, ingredients) {
+        this.title = title;
+        this.servings = servings;
+        this.ingredients = ingredients;
+        this.printRecipe = function () {
+            console.log(this.title);
+            console.log('Serves: ' + this.servings);
+            console.log('Ingredients:');
+            for (var i = 0; i < ingredients.length; i++) {
+                console.log(i + 1 + '. ' + ingredients[i]);
+            }
         };
-    };
+    }
 }
 
 var recipe = new Recipe('Rice With Peanut Butter', 4, ['rice', 'sugar', 'salt', 'peanut butter', 'milk'] );
 recipe.printRecipe();
+
+//------------------------------------------
+//2. book object
+function name(params) {
+    
+}
