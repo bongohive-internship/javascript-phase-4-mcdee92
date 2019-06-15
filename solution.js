489ffb14d724436f5abac053bbda72cf58dd9190
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-15 16:19:07 +0200

    create recipe object

--- solution.js
+++ solution.js
@@ -1,2 +1,17 @@
-// your solution should go here
-// for demo purposes use the console for any out you will need.
+//recipe object 
+function Recipe(title, servings, ingredients) {
+    this.title = title;
+    this.servings = servings;
+    this.ingredients = ingredients;
+    this.printRecipe = function () {
+        console.log(this.title);
+        console.log('Serves: ' + this.servings);
+        console.log('Ingredients:');
+        for(var i = 0; i < ingredients.length; i++) {
+            console.log(i + 1 + '. ' + ingredients[i]);
+        };
+    };
+}
+
+var recipe = new Recipe('Rice With Peanut Butter', 4, ['rice', 'sugar', 'salt', 'peanut butter', 'milk'] );
+recipe.printRecipe();
