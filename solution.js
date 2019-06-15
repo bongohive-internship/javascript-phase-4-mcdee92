ce35d722a400b50cca26e14c7796fc7310efeb47
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-15 17:17:47 +0200

    create book-array

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
 //1. recipe object 
-class Recipe {
+/*class Recipe {
     constructor(title, servings, ingredients) {
         this.title = title;
         this.servings = servings;
@@ -15,11 +15,37 @@ class Recipe {
     }
 }
 
-var recipe = new Recipe('Rice With Peanut Butter', 4, ['rice', 'sugar', 'salt', 'peanut butter', 'milk'] );
+var recipe = new Recipe('Rice With Peanut Butter', 4, ['rice', 'sugar', 'salt', 'peanut butter', 'milk']);
 recipe.printRecipe();
-
+*/
 //------------------------------------------
 //2. book object
-function name(params) {
-    
+class Book {
+    constructor(title, author, alreadyRead) {
+        this.title = title;
+        this.author = author;
+        this.alreadyRead = alreadyRead;
+    }
 }
+
+var book1 = new Book('The Hobbit','J.R.R. Tolkien', false);
+var book2 = new Book('The Fellowship of the Ring','J.R.R. Tolkien', true);
+var book3 = new Book('The Quantum Thief','Hannu Rajaniemi', true);
+var book4 = new Book('Americanah','Chimamanda Ngozi Adichie', false);
+var book5 = new Book('The Old Drift','Namwali Serpell', false);
+
+var books = [book1, book2, book3, book4, book5];
+
+books.forEach(book => {
+    console.log(book.title + ' by ' + book.author);
+});
+
+console.log('-----------------------------');
+
+books.forEach(book => {
+    if (book.alreadyRead == true) {
+        console.log('You already read "' + book.title + '" by ' + book.author);
+    } else {
+        console.log('You still need to read "' + book.title + '" by ' + book.author);
+    }
+});
